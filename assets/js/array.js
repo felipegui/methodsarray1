fa4373a6a220da56f4623b4aef2672f2afb89a64
Felipe Guimarães <fguiengenharia@gmail.com>
2021-01-11 00:28:21 -0300

    Métodos Arrays

---
+++ assets/js/array.js
@@ -0,0 +1,19 @@
+let list = ['Item 0', 'Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+
+//Remove o último item da lista
+//list.pop();
+
+//Remove o primeiro item da lista
+//list.shift();
+
+//Adicionar um item na lista
+//list.push('Item 6');
+
+//Alterar um item da lista
+//list[2] = 'Teste';
+
+//Passando os valores da variável list para um nova variável, para poder ser exibida (atualizada)
+let res = list;
+
+document.getElementById("array").innerHTML = res;
+
